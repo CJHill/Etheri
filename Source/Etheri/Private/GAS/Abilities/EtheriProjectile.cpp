9401405d0222d96811e92170ca1ee51b48b6d12a
CJHill <cameronjhill17@gmail.com>
2025-07-11 14:28:08 +0100

    Created base projectile class for Gameplay Abilities

---
+++ Source/Etheri/Private/GAS/Abilities/EtheriProjectile.cpp
@@ -0,0 +1,39 @@
+// Copyright. Hillz Studios
+
+
+#include "GAS/Abilities/EtheriProjectile.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+AEtheriProjectile::AEtheriProjectile()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false;
+
+	ProjectileSphere = CreateDefaultSubobject<USphereComponent>("Projectile Sphere");
+	SetRootComponent(ProjectileSphere);
+	ProjectileSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	ProjectileSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	ProjectileSphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	ProjectileSphere->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	ProjectileSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	ProjectileMovementComp = CreateDefaultSubobject<UProjectileMovementComponent>("Projectile Movement Component");
+	ProjectileMovementComp->InitialSpeed = 550.f;
+	ProjectileMovementComp->MaxSpeed = 550.f;
+	ProjectileMovementComp->ProjectileGravityScale = 0.f;
+}
+
+
+void AEtheriProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	ProjectileSphere->OnComponentBeginOverlap.AddDynamic(this, &AEtheriProjectile::OnSphereOverlap);
+}
+
+void AEtheriProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+
---
+++ Source/Etheri/Public/GAS/Abilities/EtheriProjectile.h
@@ -0,0 +1,33 @@
+// Copyright. Hillz Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EtheriProjectile.generated.h"
+
+
+class USphereComponent;
+class UProjectileMovementComponent;
+UCLASS()
+class ETHERI_API AEtheriProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEtheriProjectile();
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComp;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> ProjectileSphere;
+	
+};
