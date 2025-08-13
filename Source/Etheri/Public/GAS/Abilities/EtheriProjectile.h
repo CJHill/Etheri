aa974c3d7c687cf9ec6f6aa3630fba84302b20f1
CJHill <cameronjhill17@gmail.com>
2025-08-13 14:07:53 +0100

    Player faces direction they are casting firebolt in

--- Source/Etheri/Public/GAS/Abilities/EtheriProjectile.h
+++ Source/Etheri/Public/GAS/Abilities/EtheriProjectile.h
@@ -24,6 +24,7 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 private:	
